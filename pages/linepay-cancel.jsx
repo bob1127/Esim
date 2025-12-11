55d2504699175108a740d18a81107c8a33334d6b
bob1127 <61307295+bob1127@users.noreply.github.com>
2025-12-11 16:34:12 +0800

    koty

--- pages/linepay-cancel.jsx
+++ pages/linepay-cancel.jsx
@@ -2,7 +2,7 @@ export default function LinePayCancelPage() {
   return (
     <div style={{ padding: "100px", textAlign: "center" }}>
       <h1>付款已取消</h1>
-      <p>您已取消此次付款，若有問題請聯絡客服。</p>
+      <p>您已取消此次付款，若有問題請聯絡客</p>
     </div>
   );
 }
